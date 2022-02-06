dbec1fcb5939014ce3db5e6e26b0a63359a6e073
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-05 17:07:11 -0800

    updated item seed with dept id

--- pfp/migrations/0003_auto_20220206_0052.py
+++ pfp/migrations/0003_auto_20220206_0052.py
@@ -6,7 +6,7 @@ def seed(apps, schema_editor):
     Department = apps.get_model('pfp', 'Department')
     Item = apps.get_model('pfp', 'Item')
 
-    Item(department = armory, name = "Shield", description = "Protect yourself like and knight—and look like one, too.", image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F1c%2Fbe%2F0a%2F1cbe0a54d719b0a4b2f880db45ce7c28.png&f=1&nofb=1", price = 100, quantity = 250, size = "S, M, L, XL").save()
+    Item(department = 1, name = "Shield", description = "Protect yourself like and knight—and look like one, too.", image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F1c%2Fbe%2F0a%2F1cbe0a54d719b0a4b2f880db45ce7c28.png&f=1&nofb=1", price = 100, quantity = 250, size = "S, M, L, XL").save()
 
 
 class Migration(migrations.Migration):
